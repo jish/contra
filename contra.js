be645e16d1b0cfd86b57d0c41f32d37ad63933e7
Josh Lubaway <josh.lubaway@gmail.com>
2011-08-20 12:19:55 -0700

    Initial commit

---
+++ contra.js
@@ -0,0 +1,43 @@
+(function(exports) {
+
+  var Contra = exports.Contra = {};
+
+  var a = 65, b = 66;
+  var left = 37, up = 38, right = 39, down = 40;
+
+  var handlers = [];
+  var pattern = [up, up, down, down, left, right, left, right, b, a];
+  var position = 0;
+
+  Contra.check = function(evt) {
+    if (pattern[position] === evt.keyCode) {
+      position++;
+    } else {
+      position = 0;
+    }
+
+    if (position === pattern.length) {
+      Contra.win();
+      position = 0;
+    }
+  }
+
+  Contra.win = function() {
+    for(var i = 0, l = handlers.length; i<l; i++) {
+      handlers[i].call();
+    }
+  }
+
+  Contra.initialize = function() {
+    if (document.addEventListener) {
+      document.addEventListener('keyup', Contra.check);
+    }
+  }
+
+  Contra.addHandler = function(handler) {
+    handlers.push(handler);
+  }
+
+  Contra.initialize();
+
+}(this));
